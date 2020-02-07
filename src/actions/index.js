2c4206e91e903fb7f9adeb14a8e043ac94d4c0c8
Mike Wong <wongmdesign@gmail.com>
2020-02-07 16:14:54 -0500

    memoize overfetcheing

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { FETCH_POSTS, FETCH_USER, FETCH_ERROR } from "../constants";
 import jsonPlaceholder from "../api/jsonPlaceholder";
 
@@ -11,11 +12,25 @@ export const fetchPosts = () => async dispatch => {
   }
 };
 
-export const fetchUsers = id => async dispatch => {
+// export const fetchUsers = id => async dispatch => {
+//   try {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: FETCH_USER, payload: response.data });
+//   } catch (error) {
+//     dispatch({ type: FETCH_ERROR, payload: error });
+//   }
+// };
+
+// Memoize the fetches to avoid repeat fetches of same objects
+export const fetchUsers = id => dispatch => {
   try {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({ type: FETCH_USER, payload: response.data });
+    _fetchUser(id, dispatch);
   } catch (error) {
     dispatch({ type: FETCH_ERROR, payload: error });
   }
 };
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: FETCH_USER, payload: response.data });
+});
